904e692ea2d2e7722a1b4d64dc0f2d33f3c43e5b
Noppanat <pangpondzaza.1604@gmail.com>
2026-01-30 16:48:34 +0700

    Finish

--- lab15_4.cpp
+++ lab15_4.cpp
@@ -19,3 +19,16 @@ int main(){
 }
 
 //Do not modify source code above this line
+void shuffle(int &a,int &b,int &c,int &d){
+    int money[4] = {a,b,c,d};
+    for(int i=0; i<4; i++){
+        int j = rand()%4;
+        int temp = money[i];
+        money[i] = money[j];
+        money[j] = temp;
+    }
+    a = money[0];
+    b = money[1];
+    c = money[2];
+    d = money[3];
+}
